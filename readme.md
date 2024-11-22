9f2ead8f767bbb4963013707f60c040a81085ca6
andrei <dokgojungwoo@gmail.com>
2024-11-22 15:24:18 +0200

    actual commit

--- readme.md
+++ readme.md
@@ -9,3 +9,11 @@ This game will be similar to [A Dark Room](https://adarkroom.doublespeakgames.co
 After each dialogue, the player will be given two or more choices that they can make. Each choice will devolve into another branch of the story.
 
 I'll also add some idle elements.
+
+## Resources
+
+This [site](https://www.javascriptcheatsheet.org/) is a cool resource if you want a cheat sheet on JavaScript.
+
+Hello!
+
+THis is a aoscmaoscmaonfoiahjsf
--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,46 @@ function dialogueBox(text) {
   newElement.textContent = text;
 }
 
+// HTML DOM elements
+
 const valueNumber = document.querySelector('#value')
 const basicInput = document.querySelector('#input')
-const basicButton = document.querySelector('#button')
-const dialogueContainer = document.getElementById('dialogue-container')
+const nextButton = document.querySelector('#next-button')
+const dialogueContainer = document.querySelector('#dialogue-container')
+const progressBar = document.querySelector('.progress-bar')
+const coinsText = document.querySelector('#coins-text')
+
+const startButton = document.querySelector('.start-button')
+
+// game shit
+let coins = 0;
+
+function startProgress() {
+  startButton.disabled = false;
+  let width = 0;
+
+  let interval = setInterval(() => {
+    if (width >= 100) {
+      startButton.disabled = false;
+      clearInterval(interval)
+      coins++
+      coinsText.innerText = `Coins: ${coins}`
+      width = 0
+      progressBar.style.width = width + '%'
+    } else {
+      startButton.disabled = true;
+      width++
+      progressBar.style.width = width + '%'
+    }
+  }, 10)
+}
+
+document.querySelector('.start-button').addEventListener('click', startProgress);
+
+
+
+
+// text shit
 
 let currentIndex = 0;
 
@@ -16,3 +52,19 @@ const dialogues = [
   '"Take this sword. It will guide you on your journey."',
   '"Good luck, brave soul!"'
 ]
+
+const complexDialogue = [
+  {index: 1, text: 'Welcome to the game!'},
+  {index: 2, text: 'This is my second dialogue...'}
+]
+
+function showNextDialogue() {
+  if (currentIndex < complexDialogue.length)  {
+    const newDialogue = document.createElement('p');
+    newDialogue.textContent = complexDialogue[currentIndex].text;
+    dialogueContainer.appendChild(newDialogue);
+    currentIndex++;
+  }
+}
+
+nextButton.addEventListener('click', showNextDialogue)
